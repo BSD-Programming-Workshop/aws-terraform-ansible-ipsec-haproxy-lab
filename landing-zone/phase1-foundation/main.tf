a0564a9876835c6e0b0360e5c606cfaad4e29a68
Roller Angel <roller@bsd.pw>
2025-09-22 10:26:31 -0400

    Phase1: move locals block out of resource; fix init error for reserved block type

--- landing-zone/phase1-foundation/main.tf
+++ landing-zone/phase1-foundation/main.tf
@@ -24,28 +24,28 @@ provider "aws" {
   region = var.home_region
 }
 
+# Compute trusted access principals based on whether Terraform manages Control Tower
+locals {
+  base_principals = [
+    "cloudtrail.amazonaws.com",
+    "guardduty.amazonaws.com",
+    "securityhub.amazonaws.com",
+    "sso.amazonaws.com",
+    "account.amazonaws.com"
+  ]
+  ct_principals = [
+    "controltower.amazonaws.com",
+    "config.amazonaws.com",
+    "member.org.stacksets.cloudformation.amazonaws.com"
+  ]
+  computed_principals = var.enable_control_tower ? concat(local.base_principals, local.ct_principals) : local.base_principals
+}
+
 # Create AWS Organization
 resource "aws_organizations_organization" "main" {
   # Manage trusted access principals conditionally.
   # - When enable_control_tower = false (console setup/import path): keep CT/Config principals out to avoid conflicts
   # - When enable_control_tower = true  (Terraform-managed CT): include CT/Config/StackSets principals to prevent drift
-
-  locals {
-    base_principals = [
-      "cloudtrail.amazonaws.com",
-      "guardduty.amazonaws.com",
-      "securityhub.amazonaws.com",
-      "sso.amazonaws.com",
-      "account.amazonaws.com"
-    ]
-    ct_principals = [
-      "controltower.amazonaws.com",
-      "config.amazonaws.com",
-      "member.org.stacksets.cloudformation.amazonaws.com"
-    ]
-    computed_principals = var.enable_control_tower ? concat(local.base_principals, local.ct_principals) : local.base_principals
-  }
-
   aws_service_access_principals = local.computed_principals
 
   feature_set = "ALL"
