7f11921fcad6b98314ed49b872a534da65144d87
Roller Angel <roller@bsd.pw>
2025-09-22 06:51:22 -0400

    Bootstrap outputs: simplified to bucket and table names (backend.hcl is generated)

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -7,14 +7,3 @@ output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_locks.name
   description = "Name of the DynamoDB table for Terraform state locking"
 }
-
-output "backend_config" {
-  value = <<EOT
-bucket         = "${aws_s3_bucket.terraform_state.bucket}"
-key            = "terraform.tfstate"
-region         = "${var.aws_region}"
-dynamodb_table = "${aws_dynamodb_table.terraform_locks.name}"
-encrypt        = true
-EOT
-  description = "Backend configuration for main Terraform project"
-}
